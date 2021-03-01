b072226c8110aa10ac54f1aa7deb9ffd392be79b
FiestaLake <FiestaLake@protonmail.com>
2021-03-01 08:53:12 +0000

    treewide: Add build tools
    Signed-off-by: Sung Mingi <FiestaLake@protonmail.com>

---
+++ clean.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Thanks to morogoku, Arianoxx, pascua28, Tkkg1994 and djb77 for the script
+#
+# Fuesial-Kernel Clean Script
+#
+
+# Clean Build Data
+make clean
+make ARCH=arm64 distclean
+
+rm -f ./hero2lte_build.log
+rm -f ./herolte_build.log
+
+# Remove Release files
+rm -f $PWD/build/*.zip
+rm -rf $PWD/build/temp
+rm -rf $PWD/build/temp2
+rm -f $PWD/arch/arm64/configs/tmp_defconfig
+
+# Remove Created dtb
+rm -f $RDIR/arch/$ARCH/boot/dts/*.dtb
+rm -f $RDIR/arch/$ARCH/boot/dtb/*.dtb
+rm -f $RDIR/arch/$ARCH/boot/boot.img-dtb
+rm -f $RDIR/arch/$ARCH/boot/boot.img-zImage
+
+# Recreate Ramdisk Placeholders
+touch build/Q/ramdisk/apex/.placeholder
+touch build/Q/ramdisk/debug_ramdisk/.placeholder
+touch build/Q/ramdisk/dev/.placeholder
+touch build/Q/ramdisk/mnt/.placeholder
+touch build/Q/ramdisk/proc/.placeholder
+touch build/Q/ramdisk/sys/.placeholder
